9d7d0f5aca63b6e3f2d71b6432878f3ffa94fed1
Mike Bannister <notimpossiblemike@gmail.com>
2025-05-14 20:58:51 -0400

    Misc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -74,7 +74,7 @@ export default async function Home() {
       </div>
 
       <div className='w-3/4 h-full flex flex-col'>
-        <div className='h-2/5 border-b border-[var(--border)] p-4'>
+        <div className='h-2/5 pl-4 pr-4 pt-4'>
           <Textarea
             className='w-full h-full resize-none'
             placeholder='Describe your changes...'
@@ -82,42 +82,44 @@ export default async function Home() {
         </div>
 
         <div className='h-3/5 p-4 overflow-y-auto'>
-          <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
-            <FileCard
-              fileName='document.pdf'
-              fileSize={'0.22k'}
-              fileSizePercent={'3%'}
-            />
-            <FileCard
-              fileName='image.png'
-              fileSize={'1.5M'}
-              fileSizePercent={'10%'}
-            />
-            <FileCard
-              fileName='spreadsheet.xlsx'
-              fileSize={'500k'}
-              fileSizePercent={'5%'}
-            />
-            <FileCard
-              fileName='presentation.pptx'
-              fileSize={'2.3M'}
-              fileSizePercent={'20%'}
-            />
-            <FileCard
-              fileName='source.js'
-              fileSize={'50k'}
-              fileSizePercent={'1%'}
-            />
-            <FileCard
-              fileName='styles.css'
-              fileSize={'30k'}
-              fileSizePercent={'0.5%'}
-            />
-            <FileCard
-              fileName='config.json'
-              fileSize={'10k'}
-              fileSizePercent={'0.2%'}
-            />
+          <div className='p-4 border border-[var(--border)]'>
+            <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
+              <FileCard
+                fileName='document.pdf'
+                fileSize={'0.22k'}
+                fileSizePercent={'3%'}
+              />
+              <FileCard
+                fileName='image.png'
+                fileSize={'1.5M'}
+                fileSizePercent={'10%'}
+              />
+              <FileCard
+                fileName='spreadsheet.xlsx'
+                fileSize={'500k'}
+                fileSizePercent={'5%'}
+              />
+              <FileCard
+                fileName='presentation.pptx'
+                fileSize={'2.3M'}
+                fileSizePercent={'20%'}
+              />
+              <FileCard
+                fileName='source.js'
+                fileSize={'50k'}
+                fileSizePercent={'1%'}
+              />
+              <FileCard
+                fileName='styles.css'
+                fileSize={'30k'}
+                fileSizePercent={'0.5%'}
+              />
+              <FileCard
+                fileName='config.json'
+                fileSize={'10k'}
+                fileSizePercent={'0.2%'}
+              />
+            </div>
           </div>
         </div>
       </div>
