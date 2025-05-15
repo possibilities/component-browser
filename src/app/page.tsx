d2bc85af4b75eb086fa2c5643ca82a1840eb5205
Mike Bannister <notimpossiblemike@gmail.com>
2025-05-14 22:05:05 -0400

    Misc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import { Button } from '@/app/components/ui/button'
 import { Textarea } from '@/app/components/ui/textarea'
 import { exec } from 'child_process'
 import { promisify } from 'util'
-import { Folder } from 'lucide-react'
+import { Folder, ChevronRight } from 'lucide-react'
 
 const execAsync = promisify(exec)
 
@@ -86,7 +86,14 @@ export default async function Home() {
         <div className='h-3/5 p-4 overflow-y-auto'>
           <div className='p-4 border border-[var(--border)] h-full'>
             <div className='mb-4 flex gap-3 items-center px-3 py-1.5 border border-[var(--border)] justify-between text-muted-foreground text-base'>
-              <Folder className='h-5 w-5' />
+              <div className='flex gap-3 items-center'>
+                <ChevronRight
+                  className={`h-5 w-5 transition-transform duration-200 ${
+                    true ? 'rotate-90' : ''
+                  }`}
+                />
+                <Folder className='h-5 w-5' />
+              </div>
               22k (0.5%)
             </div>
             <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
